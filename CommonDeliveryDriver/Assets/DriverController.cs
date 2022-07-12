dae43e1245b91728605e3600b5bc85bb47d47c9b
NaibOfDesert <tryc.artur@gmail.com>
2022-07-13 00:01:35 +0200

    Unity template files, move testing.

---
+++ CommonDeliveryDriver/Assets/DriverController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriverController : MonoBehaviour
+{
+    [Header("Driver Variables")]
+    // Testing - SerializeField
+    [SerializeField] float steerSpeed = 1f;
+    public float moveSpeed;
+    //
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, -(Input.GetAxis("Horizontal") * steerSpeed));
+        transform.Translate(0, (Input.GetAxis("Vertical") * moveSpeed), 0);
+    }
+}
