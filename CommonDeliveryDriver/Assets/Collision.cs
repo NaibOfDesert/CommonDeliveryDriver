bf3949b40a7a94f870e3e0febf2bbe17fb9da82e
NaibOfDesert <tryc.artur@gmail.com>
2022-07-16 19:59:19 +0200

    Environment.

--- CommonDeliveryDriver/Assets/Collision.cs
+++ CommonDeliveryDriver/Assets/Collision.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D other) 
+    private void OnCollisionEnter2D(Collision2D collider) 
     {
         Debug.Log("Collision");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) 
+    {
+        Debug.Log("Collider - trigger!");
+    }
+
+    private void OnTriggerExit2D(Collider2D collider) 
+    {
+         
     }
 }
